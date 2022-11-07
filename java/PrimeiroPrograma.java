b15fffc63d0ee85bf201e534d5f4b42efe1e5296
Elder <elder.soares52@gmail.com>
2022-11-07 10:50:46 -0400

    commit inicial

---
+++ java/PrimeiroPrograma.java
@@ -0,0 +1,23 @@
+package dio.java;
+
+import dio.java.model.Gato;
+
+public class PrimeiroPrograma {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	Gato gato = new Gato();
+	Livros livros = new Livros();
+	System.out.println(gato);
+	System.out.println(livros);
+		
+	}
+}
+
+class Livros {
+	private String nome;
+	private String npag;
+}
+
